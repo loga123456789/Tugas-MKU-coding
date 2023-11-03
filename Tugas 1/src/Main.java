345df286eb364b54744ff848e4f4b036b6327504
loga123456789 <147137907+loga123456789@users.noreply.github.com>
2023-11-03 16:54:29 +0700

    Add files via upload
    Tugas Coding Loga Rizal Pratama NPM:G1A023008

---
+++ Tugas 1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+            if (i <= 9) {
+                System.out.println(i);
+            } else {
+                System.out.println("Loga Rizal Pratama");
+            }
+
+
+        }
+    }
+}
